b23e62b8492a56caaf3b8a90a8c5a4eed200f772
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-03-02 11:20:45 +0100

    init: initial commit for home-manager configuration

---
+++ home.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "joshuachp";
+  home.homeDirectory = "/home/joshuachp";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+      neovim
+      git
+      chezmoi
+      tmux
+      gopass
+      starship
+
+      exa
+      bat
+      ripgrep
+
+      clang
+      nodejs
+      clang-tools
+  ];
+  programs.alacritty.enable = true;
+}
